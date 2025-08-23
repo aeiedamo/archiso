596267174c0861b6c429f885a807a8ed49061e14
AlaEldin Mohamed <aeiedamo@gmail.com>
2025-08-23 13:19:51 +0300

    Version 0

---
+++ main/airootfs/root/.bash_profile
@@ -0,0 +1,2 @@
+# Set EDITOR
+export EDITOR=vim
---
+++ main/profiledef.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="arch"
+iso_label="ARCHLINUX"
+iso_publisher="aeiedamo"
+iso_application="My Arch Linux ISO configuration"
+iso_version="linux"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking')
+bootstrap_tarball_compression=(zstd -c -T0 --long -19)
